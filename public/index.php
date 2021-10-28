8f233df38d18e48f9a8d2986c14a2fa9666c6812
Matilda Jönsson <matilda.jonsson4@learnet.se>
2021-10-28 09:35:07 +0200

    footer

--- public/index.php
+++ public/index.php
@@ -45,27 +45,28 @@ $_SITE_TITLE = 'Module_4_Database'
         <div class="bg-container rounded-lg w-full">s</div>
     </main>
 
-    <footer style="box-shadow:inset 0px 2px 10px #3B82F6;" class="bg-gray-600 rounded-t-xl h-24">
+    <footer style="box-shadow:inset 0px 2px 10px 0px #3B82F6;" class="flex flex-row gap-20 bg-gray-600 rounded-t-xl py-7 pl-20 pr-20">
 
-        <div class="flex flex-row gap-20  mt-7 ml-20 mr-20">
-
-            <div class="w-1/5 mx-5 ml-10">
-                <h2 class="text-2xl text-blue-500 pb-2">Products</h2>
+            <div class=" mx-5 ml-10 flex-1">
+                <h2 class="text-xl text-blue-500 pb-2">Products</h2>
                 <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Store</a></p>
                 <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Cart</a></p>
+                <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Liked</a></p>
             </div>
-            <div class="w-1/5 ">
-                <h2 class="text-2xl text-blue-500 pb-2">Account</h2>
+            <div class=" flex-1">
+                <h2 class="text-xl text-blue-500 pb-2">Account</h2>
                 <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Login</a></p>
                 <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Register</a></p>
-            </div>
-            <div class="w-1/5">
-                <h2 class="text-2xl text-blue-500 pb-2">About</h2>
                 <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Account</a></p>
             </div>
-            
+            <div class=" flex-1">
+                <h2 class="text-xl text-blue-500 pb-2">About</h2>
+                <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">How it works</a></p>
+            </div>
+            <div class="flex-1">
+            <i style="color: #3B82F6;" class="fas fa-cars fa-4x"></i><!-- Logo -->
+            </div>
 
-        </div>
 
     </footer>
 
