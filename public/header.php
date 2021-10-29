e3b50ac882c669b63ce021c0e8b2b1d27f832992
Matilda Jönsson <matilda.jonsson4@learnet.se>
2021-10-29 09:59:40 +0200

    included header n footer

---
+++ public/header.php
@@ -0,0 +1,20 @@
+<header class="bg-background text-gray-400 flex flex-col h-36 px-6 md:px-24 lg:px-26 xl:px-36 2xl:px-80">
+    <nav class="flex flex-row justify-between my-auto">
+        <!-- Left -->
+        <div class="flex flex-row gap-16 text-lg my-auto">
+            <a href="index.php" class="text-gray-400 hover:text-content">Home</a>
+            <a href="#" class="text-gray-400 hover:text-content">About</a>
+            <a href="#" class="text-gray-400 hover:text-content">Store</a>
+        </div>
+        <!-- Right -->
+        <div class="flex flex-row gap-6 text-md">
+            <a href="login.php" class="py-2 hover:text-content">Login</a>
+            <a href="#" class="bg-blue-500 px-6 py-2 rounded-md text-blue-200 font-medium hover:bg-blue-600">Register</a>
+        </div>
+
+        <div class="hidden flex flex-row gap-6 text-md">
+            <a href="#" class="text-highlight py-2 my-auto hover:text-content">John Doe</a>
+            <img class="w-16 rounded-full border border-content" src="https://i.pravatar.cc/100">
+        </div>
+    </nav>
+</header>
--- public/index.php
+++ public/index.php
@@ -20,26 +20,11 @@ $_SITE_TITLE = 'Module_4_Database'
 </head>
 <body class="min-h-screen flex flex-col">
 
-    <header class="bg-background text-gray-400 flex flex-col h-36 px-6 md:px-24 lg:px-26 xl:px-36 2xl:px-80">
-        <nav class="flex flex-row justify-between my-auto">
-            <!-- Left -->
-            <div class="flex flex-row gap-16 text-lg my-auto">
-                <a href="#" class="text-gray-400 hover:text-content">Home</a>
-                <a href="#" class="text-gray-400 hover:text-content">About</a>
-                <a href="#" class="text-gray-400 hover:text-content">Store</a>
-            </div>
-            <!-- Right -->
-            <div class="flex flex-row gap-6 text-md">
-                <a href="login.php" class="py-2 hover:text-content">Login</a>
-                <a href="#" class="bg-blue-500 px-6 py-2 rounded-md text-blue-200 font-medium hover:bg-blue-600">Register</a>
-            </div>
-
-            <div class="hidden flex flex-row gap-6 text-md">
-                <a href="#" class="text-highlight py-2 my-auto hover:text-content">John Doe</a>
-                <img class="w-16 rounded-full border border-content" src="https://i.pravatar.cc/100">
-            </div>
-        </nav>
-    </header>
+    <?php
+    
+        include 'header.php';
+    
+    ?>
 
     <main class="bg-background flex flex-grow px-6 md:px-24 lg:px-26 xl:px-36 2xl:px-80">
         <div class="bg-container rounded-lg w-full">s</div>
@@ -101,32 +86,11 @@ $_SITE_TITLE = 'Module_4_Database'
 
     <br><br>
 
-    <footer style="box-shadow:inset 0px 2px 10px 0px #3B82F6;" class="flex flex-row gap-20 bg-gray-600 rounded-t-xl py-7 pl-20 pr-20">
-
-        <div class=" mx-5 ml-10 flex-1">
-            <h2 class="text-xl text-blue-500 pb-2">Products</h2>
-            <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Store</a></p>
-            <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Cart</a></p>
-            <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Liked</a></p>
-        </div>
-
-        <div class=" flex-1">
-            <h2 class="text-xl text-blue-500 pb-2">Account</h2>
-            <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Login</a></p>
-            <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Register</a></p>
-            <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">Account</a></p>
-        </div>
-
-        <div class=" flex-1">
-            <h2 class="text-xl text-blue-500 pb-2">About</h2>
-            <p class="text-xs text-gray-200 hover:text-blue-500"><a href="#">How it works</a></p>
-        </div>
-
-        <div class="flex-1">
-        <i style="color: #3B82F6;" class="fas fa-cars fa-4x"></i><!-- Logo -->
-        </div>
-            
-    </footer>
+    <?php
+    
+        include 'footer.php';
+    
+    ?>
 
     <script>
 
