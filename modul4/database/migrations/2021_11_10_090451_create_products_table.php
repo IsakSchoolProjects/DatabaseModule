1ea2f43770a21eda84679bf5d41b02e7b3dd2521
Matilda Jönsson <matilda.jonsson4@learnet.se>
2021-11-10 10:55:13 +0100

    databse

--- modul4/database/migrations/2021_11_10_090451_create_products_table.php
+++ modul4/database/migrations/2021_11_10_090451_create_products_table.php
@@ -13,10 +13,10 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('productss', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        // Schema::create('productss', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
     }
 
     /**
@@ -26,6 +26,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('productss');
+        // Schema::dropIfExists('productss');
     }
 }
