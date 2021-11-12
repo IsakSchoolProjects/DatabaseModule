8c5285019f030b260393c3112f69f56b8ab9a127
Sebastian Ljungkrantz <sebastian.ljungkran@learnet.se>
2021-11-12 11:32:21 +0100

    Working cart

--- modul4/app/Http/Controllers/ProductsController.php
+++ modul4/app/Http/Controllers/ProductsController.php
@@ -10,9 +10,41 @@ use Illuminate\Support\Facades\DB;
 
 class ProductsController extends Controller
 {
-    function list(){
+    function showAllProducts(){
         $res = DB::table('products')->get();
 
-        return view('pages.list')->with('res', $res);
+        return view('pages.store')->with('res', $res);
+    }
+
+    function showCartItems() {
+        $account_id = 1;
+
+        $cart_products = DB::select(DB::raw("SELECT id, product_id FROM cart WHERE account_id = $account_id"));
+
+        $has_cart_items = sizeof($cart_products) > 0;
+
+        $cart_product_res = [];
+        $cart_product_ids = [];
+
+        for ($x = 0; $x < sizeof($cart_products); $x++) {
+            array_push($cart_product_res, $cart_products[$x]->product_id);
+            array_push($cart_product_ids, $cart_products[$x]->id);
+        }    
+
+        $products = [];
+
+        if(sizeof($cart_products) > 0) {
+            $stringified_cart_product_res = substr(json_encode($cart_product_res), 1, -1);
+
+            $products = DB::select(DB::raw("SELECT * FROM products WHERE id IN ($stringified_cart_product_res)"));    
+        }
+        
+        $data = array(
+            'products'=> $products,
+            'cart_products_ids'=> $cart_product_ids,
+            'has_cart_items'=> $has_cart_items
+        );
+
+        return view('pages.cart')->with('data', $data);
     }
 }
